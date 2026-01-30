28b71d7ada3831e06d10d3060fa6ee36bd24a0a8
Antoine <rousselotantoine7@gmail.com>
2026-01-30 09:42:43 +0100

    frontend structured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,9 @@
-import { useAuth } from './context/AuthContext';
-import Header from './components/Header';
-import DailyForm from './components/DailyForm';
+
 
 function App() {
-  const { user } = useAuth();
-
-  if (!user) {
-    return <div className="p-10 text-center">Page de Login à isoler aussi !</div>;
-  }
-
   return (
-    <div className="min-h-screen bg-dark pb-20">
-      <div className="max-w-md mx-auto px-6">
-        <Header />
-        
-        <main className="space-y-8 mt-4">
-          {/* Section 1 : Saisie */}
-          <DailyForm />
-
-          {/* Section 2 : Résumé du jour (À venir) */}
-          {/* Section 3 : Flux (À venir) */}
-          {/* Section 4 : Stats (À venir) */}
-        </main>
-      </div>
+    <div>
+      <h1>App</h1>
     </div>
   );
 }
